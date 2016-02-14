34149d493748a6817a6a08ac04bafef616aca362
Hannah L. Johnson <hlj.inbox@gmail.com>
2016-02-14 17:25:14 -0600

    Update ClockDesigns.java comments to reflect Javadoc formatting

--- HourClock/ClockDesigns.java
+++ HourClock/ClockDesigns.java
@@ -1,10 +1,15 @@
 /**
-  clockDesigns enumerated type definition. Used to differentiated between different clock face designs
+* clockDesigns enumerated type definition. Used to differentiated between different clock face designs
+* 
+* @author Hannah Johnson, Rabel Marte, Ashley Hutton
+*/
+
+
+/**  
   @pre: None
   @post: None
   @return: None
 */
-
 enum clockDesigns
 {
   BLACK, GREEN, BLUE, PINK, PIZZA
